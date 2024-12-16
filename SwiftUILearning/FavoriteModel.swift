ff79443cd87e6ee32395b7fad61d981790aea773
Yakup Kavak <yakupkavk@gmail.com>
2024-12-16 10:44:07 +0300

    feat: add navigate

---
+++ SwiftUILearning/FavoriteModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteModel.swift
+//  SwiftUILearning
+//
+//  Created by Yakup Kavak on 16.12.2024.
+//
+
+import Foundation
---
+++ SwiftUILearning/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SwiftUILearning
+//
+//  Created by Yakup Kavak on 15.12.2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
